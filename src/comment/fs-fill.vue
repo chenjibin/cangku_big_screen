581b418e347bf288634752fce6bbcbbd3ed2e79d
chenjibin <363840817@qq.com>
2017-11-05 23:06:14 +0800

    css add

---
+++ src/comment/fs-fill.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="num-fill-component">
+    <div class="progress" ref="progress"></div>
+  </div>
+</template>
+<style lang="less">
+  .num-fill-component {
+    position: relative;
+    width: 100px;
+    margin: 0 auto;
+    overflow: hidden;
+    border:1px solid #0bb79e;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    background-color: rgba(10, 2, 47, 0.5);
+    .progress {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height:0;
+      background-color: rgba(11, 183, 158, 0.8);
+      transition: height 0.5s ease-in-out;
+    }
+  }
+</style>
+<script>
+    export default{
+        props: {
+          target: Number,
+          now: Number,
+          height: {
+            type: Number,
+            default: 500
+          }
+        },
+        data() {
+            return {
+
+            }
+        },
+        mounted() {
+          this.$el.style.height = this.height + 'px'
+          this._setProgressHeight()
+        },
+        methods: {
+          _setProgressHeight() {
+            let radio = (this.now / this.target).toFixed(2)
+            let eHeight = this.height * radio
+            setTimeout(() => {
+              this.$refs.progress.style.height = eHeight * radio + 'px'
+            }, 20)
+          }
+        }
+    }
+</script>
--- src/components/warehouse.vue
+++ src/components/warehouse.vue
@@ -39,45 +39,16 @@
     </div>
     <div class="center-area">
       <div class="number-block">
-        <p class="number">150,000,000</p>
+        <vue-countup class="number" :start="0" :end="150000000"></vue-countup>
         <div class="line"></div>
         <p class="desc">今日出货总量</p>
       </div>
       <div class="all-show-block flex-box">
-        <div class="each-block flex-one">
-          <p class="t-number">50000</p>
-          <div class="num-fill-component">
-            <div class="progress"></div>
-          </div>
+        <div class="each-block flex-one" v-for="item in warehouseData">
+          <p class="t-number">{{item.target}}</p>
+          <fs-fill :target="item.target" :now="item.now" :height="500"></fs-fill>
           <div class="warehouse-name">
-            <span>8号仓</span>
-          </div>
-        </div>
-        <div class="each-block flex-one">
-          <p class="t-number">50000</p>
-          <div class="num-fill-component">
-
-          </div>
-          <div class="warehouse-name">
-            <span>9号仓</span>
-          </div>
-        </div>
-        <div class="each-block flex-one">
-          <p class="t-number">50000</p>
-          <div class="num-fill-component">
-
-          </div>
-          <div class="warehouse-name">
-            <span>10号仓</span>
-          </div>
-        </div>
-        <div class="each-block flex-one">
-          <p class="t-number">50000</p>
-          <div class="num-fill-component" style="height: 350px;">
-
-          </div>
-          <div class="warehouse-name">
-            <span>实体专卖</span>
+            <span>{{item.name}}</span>
           </div>
         </div>
       </div>
@@ -159,22 +130,37 @@
 </template>
 
 <script>
-  import VueEcharts from '../comment/vue-echarts.vue'
   import 'echarts'
+  import VueEcharts from 'vue-echarts'
+  import VueCountup from '@/comment/vue-countup'
+  import FsFill from '@/comment/fs-fill'
 
 export default {
   name: 'warehouse',
   data () {
     return {
       updateTime: '2017-11-11 22:00:00',
-      testData: [
-        {name: '2016/12/18 6:38:08', value: ['2016/12/18 6:38:08', 80]},
-        {name: '2016/12/18 16:18:18', value: ['2016/12/18 16:18:18', 60]},
-        {name: '2016/12/18 19:18:18', value: ['2016/12/18 19:18:18', 90]}
-      ],
-      anchor: [
-        {name: '2016/12/18 00:00:00', value: ['2016/12/18 00:00:00', 0]},
-        {name: '2016/12/19 00:00:00', value: ['2016/12/19 00:00:00', 0]}
+      warehouseData: [
+        {
+          name: '8号仓',
+          target: 50000,
+          now: 30000
+        },
+        {
+          name: '9号仓',
+          target: 50000,
+          now: 30000
+        },
+        {
+          name: '10号仓',
+          target: 50000,
+          now: 30000
+        },
+        {
+          name: '实体专卖',
+          target: 50000,
+          now: 30000
+        }
       ],
       sendOrder: [
         {
@@ -235,7 +221,7 @@ export default {
           }
         },
         xAxis: {
-          type: 'time',
+          type: 'category',
           boundaryGap: false,
           axisLine: {
             lineStyle: {
@@ -249,7 +235,8 @@ export default {
           },
           nameTextStyle: {
             color: 'rgb(255,255,255)'
-          }
+          },
+          data: ['8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00']
         },
         yAxis: {
           type: 'value',
@@ -273,20 +260,12 @@ export default {
             name: '完成发货速率',
             type: 'line',
             stack: '总量',
-            data: this.testData,
             lineStyle: {
               normal: {
                 color: '#ffd702'
               }
-            }
-          },
-          {
-            name: '.anchor',
-            type: 'line',
-            showSymbol: false,
-            data: this.anchor,
-            itemStyle: {normal: {opacity: 0}},
-            lineStyle: {normal: {opacity: 0}}
+            },
+            data: [2, 7, 10, 18, 14, 18, 20, 30, 10, 18, 33, 24, 20, 19, 39]
           }
         ]
       }
@@ -299,31 +278,15 @@ export default {
 
   },
   components: {
-    VueEcharts
+    VueEcharts,
+    VueCountup,
+    FsFill
   }
 }
 </script>
 
 <style scoped lang="less">
-.num-fill-component {
-  position: relative;
-  width: 100px;
-  margin: 0 auto;
-  height: 500px;
-  overflow: hidden;
-  border:1px solid #0bb79e;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
-  background-color: rgba(10, 2, 47, 0.5);
-  .progress {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height:300px;
-    background-color: rgba(11, 183, 158, 0.8);
-  }
-}
+
 .warehouse {
   padding-top: 127px;
   width: 100%;
